1f4514667df0c9ccffd651a8d17c53b29358e040
Akhil Casey <akhilkc619@gmail.com>
2019-06-02 17:12:10 +0530

    Pom changes and restController import

--- src/main/java/com/casey/controller/DoctorController.java
+++ src/main/java/com/casey/controller/DoctorController.java
@@ -1,11 +1,12 @@
 package com.casey.controller;
 
-import org.springframework.stereotype.Controller;
-
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController {
 
+	//all data doctor wants to be here..
+	
 }
