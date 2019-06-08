66f7ea9d8c63fc9816e277554964558a72c58cdf
Akhil Casey <akhilkc619@gmail.com>
2019-06-08 18:09:09 +0530

    Build with interface and its implementation

--- src/main/java/com/casey/controller/AdminController.java
+++ src/main/java/com/casey/controller/AdminController.java
@@ -9,20 +9,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.casey.bean.PatientDTO;
-import com.casey.repository.PatientRepository;
+import com.casey.implementation.AdminServiceImplementation;
 import com.casey.utils.StatusResponse;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
-	@Autowired // spring DI , inheriting the class without creating an instance here..
-	PatientRepository patientRepo;
+	@Autowired
+	AdminServiceImplementation adminServiceImpl;
 
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
 	public StatusResponse getAllPatientData() {
-		// boolean status = patientRepo.findAll();
-		List<PatientDTO> data = patientRepo.findAll();
+		List<PatientDTO> data = adminServiceImpl.getAllPatientData();
 		StatusResponse status = new StatusResponse(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST, "fail", null);
 		if (data != null) {
 			status = new StatusResponse(HttpStatus.OK, HttpStatus.OK, "success", data);
